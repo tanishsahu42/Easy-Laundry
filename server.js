fae6297dea26e9fe289a0393b380324175315965
tanish-ism <tanishsahu42.19je0854@me.iitism.ac.in>
2021-10-01 21:50:42 +0530

    build static things

--- server.js
+++ server.js
@@ -6,17 +6,30 @@ const expressLayout = require('express-ejs-layouts')
 const PORT=process.env.PORT || 3000
 
 app.use(express.static('public'))
-app.get('/',(req,res)=>
-{
-    res.render('home')
-})
+
 
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 
+app.get('/',(req,res)=>
+{
+    res.render('home')
+})
+app.get('/cart',(req,res)=>
+{
+    res.render('customers/cart')
+})
 
+app.get('/login',(req,res)=>
+{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>
+{
+    res.render('auth/register')
+})
 
 
 app.listen(PORT,()=>
